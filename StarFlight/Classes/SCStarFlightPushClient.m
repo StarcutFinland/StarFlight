8423169e0d07286e16c13fffd065e0dcfafa5fcc
Ankush Kushwaha <ankush.kushwaha@starcut.com>
2018-03-26 14:26:12 +0300

    Remove unnecessory line/spaces and improve syntax

--- StarFlight/Classes/SCStarFlightPushClient.m
+++ StarFlight/Classes/SCStarFlightPushClient.m
@@ -46,33 +46,30 @@ NSString *const SCStarFlightClientUUIDNotification = @"com.starcut.starflight.cl
     [self registerWithToken:token clientUUID:nil tags:tags timePreferences:nil];
 }
 
-- (void)registerWithToken:(NSString *)token
-               clientUUID:(NSString *)clientUUID
-                     tags:(NSArray<NSString *> *)tags
-          timePreferences:(NSDictionary *)timePreferencesDict
+- (void)registerWithToken:(NSString *)token clientUUID:(NSString *)clientUUID tags:(NSArray<NSString *> *)tags timePreferences:(NSDictionary *)timePreferencesDict
 {
     if (self.appID && self.clientSecret && token)
     {
         NSString *post = [NSString stringWithFormat:@"action=register&appId=%@&clientSecret=%@&type=ios&token=%@", self.appID, self.clientSecret, token];
-        
+
         if (clientUUID && ![clientUUID  isEqualToString: @""])
         {
             post = [post stringByAppendingString:[NSString stringWithFormat:@"&clientUuid=%@", clientUUID]];
         }
-        
+
         NSString *tagsToRegister = @"";
         for (NSString *tag in tags)
         {
             tagsToRegister = [tagsToRegister stringByAppendingString:[NSString stringWithFormat:@",%@", tag]];
         }
-        
+
         tagsToRegister = [tagsToRegister stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@", "]];
-        
+
         if (tagsToRegister && ![tagsToRegister isEqualToString:@""])
         {
             post = [post stringByAppendingString:[NSString stringWithFormat:@"&tags=%@", tagsToRegister]];
         }
-        
+
         if (timePreferencesDict) {
             NSData *jsonData = [NSJSONSerialization dataWithJSONObject:timePreferencesDict options:NSJSONWritingPrettyPrinted error:nil];
             
@@ -81,11 +78,11 @@ NSString *const SCStarFlightClientUUIDNotification = @"com.starcut.starflight.cl
             post = [post stringByAppendingString:[NSString stringWithFormat:@"&timePreferences=%@", timePreferencesJsonString]];
             
         }
-        
+
         // time
         NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
         NSString *postLength = [NSString stringWithFormat:@"%lu", (unsigned long)[postData length]];
-        
+
         NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
         [request setURL:[self starFlightAPIURL]];
         [request setHTTPMethod:@"POST"];
@@ -93,16 +90,16 @@ NSString *const SCStarFlightClientUUIDNotification = @"com.starcut.starflight.cl
         [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
         [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
         [request setHTTPBody:postData];
-        
+
         self.connection = [[SCHTTPNetwork alloc] init];
         [self.connection startResquest:request success:^(NSData *responseData) {
             NSDictionary *responseObject = [NSJSONSerialization JSONObjectWithData:responseData options: NSJSONReadingMutableContainers error:NULL];
-            
+
             if (responseObject)
             {
                 [[NSNotificationCenter defaultCenter] postNotificationName:SCStarFlightClientUUIDNotification object:self userInfo:responseObject];
             }
-            
+
         } failure:^(NSError *error, NSData *responseObject) {
             NSLog(@"%@", [error localizedDescription]);
         }];
